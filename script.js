07a60b9b4089c93507c2f5a7e78946cdc6fc09d0
NaderJS <legandryeagle@gmail.com>
2017-11-05 23:25:40 +0200

    bug fix

---
+++ script.js
@@ -0,0 +1,77 @@
+new Vue({
+    el:'#app',
+    data:{
+     height: (window.innerHeight - 60) +'px',
+     socialArray:[
+         {
+             src:'./src/imgs/facebook.ico',
+             link:'https://www.facebook.com/profile.php?id=100008930282377'
+         },{
+             src:'./src/imgs/linkedin.ico',
+             link:'https://www.linkedin.com/in/nader-atef-6806b2139/'
+         },{
+             src:'./src/imgs/google_plus.ico',
+             link:'https://plus.google.com/u/0/118143558688056899911'
+         }
+     ],
+     projects:[
+         {
+             img:'./src/imgs/tictac.png',
+             link:'https://naderjs.lib.id/ticvue@dev/',
+             codeLink:'https://github.com/NaderJS/libticVue',
+             name:'Undefeatable tictactoe game',
+             id:'pro1'
+         },{
+             img:'./src/imgs/pomo.png',
+             link:'https://naderjs.github.io/Pomodro',
+             codeLink:'https://github.com/NaderJS/Pomodro/',
+             name:'a Pomodoro clock',
+             id:'pro2'
+         },{
+             img:'./src/imgs/twitch.png',
+             link:'https://codepen.io/Nader_Camper/full/qmGYZd',
+             codeLink:'https://codepen.io/Nader_Camper/pen/qmGYZd',
+             name:'Twitch streams status app',
+             id:'pro3'
+         },{
+             img:'./src/imgs/simgame.png',
+             link:'https://naderjs.github.io/simgame/',
+             codeLink:'https://github.com/NaderJS/simgame',
+             name:'a simple simon game',
+             id:'pro4'
+         },{
+             img:'./src/imgs/notes.png',
+             link:'https://naderjs.lib.id/notesApp@dev/',
+             codeLink:'https://github.com/NaderJS/notesapp',
+             name:'Notes App',
+             id:'pro5'
+         }
+     ],
+     codeArray:[
+         {
+             img:'./src/imgs/fccIcon.jpg',
+             link:'https://www.freecodecamp.com/naderjs/'
+         },{
+             img:'./src/imgs/github.png',
+             link:'https://github.com/NaderJS/'
+         },{
+             img:'./src/imgs/codepen.png',
+             link:'https://codepen.io/Nader_Camper/'
+         }
+     ],
+     form:{}
+    },
+    methods:{
+
+    },
+    mounted:function(){
+      let self = this
+      self.el=document.getElementById('about');
+
+    },
+    computed:{
+        scrControl:function(){
+            console.log(window)
+        }
+    }
+})
